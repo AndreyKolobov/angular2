83e7f84481672d747c31f9f72a54bfcc0ab4657b
AndreyKolobov <kolobov.andrew@gmail.com>
2016-12-30 17:04:51 -0800

    added routing to the project

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -5,16 +5,24 @@ import { Ingredient } from '../ingredient';
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very Tasty', 'http://www.bbcgoodfood.com/sites/default/files/styles/carousel_small/public/recipe/recipe-image/2016/03/chicken-schnitzel-strips-with-tomato-spaghetti.jpg?itok=3r23piTE', [
+    new Recipe('Schnitzel', 'Very Tasty', 'http://toriavey.com/images/2011/02/IMG_1544.jpg', [
     new Ingredient('French Fries', 2), new Ingredient('Pork Meat', 1)
     ]),
     new Recipe('Summer Salad', 'Okayish', 'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg', [])
   ];
 
   constructor() { }
-  
+
   getRecipes(){
     return this.recipes;
+  }
+
+  getRecipe(id: number){
+    return this.recipes[id];
+  }
+
+  deleteRecipe(recipe: Recipe){
+    this.recipes.splice(this.recipes.indexOf(recipe), 1);
   }
 
 }
